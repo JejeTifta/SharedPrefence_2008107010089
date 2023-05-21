da16eb6d674d429d2f284f639323075e868c524e
JejeTifta <102013411+JejeTifta@users.noreply.github.com>
2023-05-21 12:37:18 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/alarmfile/MainActivity.kt
+++ app/src/main/java/com/example/alarmfile/MainActivity.kt
@@ -6,11 +6,11 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.example.alarmfile.databinding.ActivityMainBinding
-
+//membuat class untuk Main
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
-
+//implementasi kedalam folder layout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.buttonOpen.setOnClickListener(this)
         binding.buttonSave.setOnClickListener(this)
     }
-
+//function onclik button view data
     override fun onClick(view: View) {
         when (view.id){
             R.id.button_new -> newFile()
@@ -28,13 +28,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.button_save -> saveFile()
         }
     }
-
+//function button set new file
     private fun newFile() {
         binding.editTitle.setText("")
         binding.editFile.setText("")
         Toast.makeText(this, "Clearing file", Toast.LENGTH_SHORT).show()
     }
-
+//function button show list yg telah di save berdasarkan judul
     private fun showList() {
         val items = fileList()
         val builder = AlertDialog.Builder(this)
@@ -43,13 +43,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val alert = builder.create()
         alert.show()
     }
+    //mengambil data sting dari file name yg telah di save
     private fun loadData(title: String) {
         val fileModel = FileHelper.readFromFile(this, title)
         binding.editTitle.setText(fileModel.filename)
         binding.editFile.setText(fileModel.data)
         Toast.makeText(this, "Loading " + fileModel.filename + " data", Toast.LENGTH_SHORT).show()
     }
-
+//menyimpan data yg telah di input
     private fun saveFile() {
         when {
             binding.editTitle.text.toString().isEmpty() -> Toast.makeText(this, "Judul harus diisi terlebih dahulu", Toast.LENGTH_SHORT).show()
